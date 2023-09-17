c12819773f1a48bcf90d72e967f992870bc03b79
Pelidyai <dima-pile@mail.ru>
2023-09-17 15:11:22 +0700

    Init workspace

---
+++ jser/niche/commission/serialize_niche_commission.py
@@ -0,0 +1,9 @@
+
+
+def main():
+    # TODO serialize to bin file. Source (csv, json, txt and other) should be in this module 'jser.niche.commission'
+    pass
+
+
+if __name__ == '__main__':
+    main()
