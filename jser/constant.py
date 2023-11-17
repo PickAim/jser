32fe01f4b1d04ebc5cdb82186edf22e494201a46
Pelidyai <dima-pile@mail.ru>
2023-11-17 22:46:05 +0700

    Hard code paths to make it possible to run on JDU. Raise version (v0.0.5)

--- jser/constant.py
+++ jser/constant.py
@@ -18,16 +18,13 @@ def get_path(dir_to_search: str, file_to_search: str):
         return os.path.join(dir_to_search, file_to_search)
 
 
-COMMISSION_WILDBERRIES_CSV: Final[str] = get_path(file_dir, 'WildberriesInput/niche_commission.csv')
+COMMISSION_WILDBERRIES_CSV: Final[str] = get_path(file_dir, 'niche/commission/Wildberries/WildberriesInput/niche_commission.csv')
 COMMISSION_WILDBERRIES_BINARY: Final[str] = get_path(file_dir,
                                                      'niche/commission/Wildberries/WildberriesOutput/niche_commission.pickle')
 
 WAREHOUSE_WILDBERRIES_JSON: Final[str] = get_path(file_dir, 'data_warehouse.json')
 WAREHOUSE_WILDBERRIES_BINARY: Final[str] = get_path(file_dir,
                                                     'warehouse/information/Wildberries/WildberriesOutput/warehouse_data.pickle')
-
-COMMISSION_KEY: Final[str] = "commission"
-RETURN_PERCENT_KEY: Final[str] = "return_percent"
 
 
 class HandlerType(Enum):
