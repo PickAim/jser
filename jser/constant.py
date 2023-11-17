6a7f6f63c53241bc9101b11a4af647ea2313afe9
Pelidyai <dima-pile@mail.ru>
2023-11-17 23:04:47 +0700

    Move files and use relative paths to make it possible to run on JDU. Raise version (v0.0.6)

--- jser/constant.py
+++ jser/constant.py
@@ -8,7 +8,9 @@ RETURN_PERCENT_KEY: Final[str] = "return_percent"
 
 WILDBERRIES_NAME: Final[str] = 'wildberries'
 
-file_dir: str = os.path.dirname(os.path.abspath(__file__))
+splitted: list[str] = os.getcwd().split(os.sep)[:-1]
+splitted[0] += os.sep
+file_dir: str = os.path.join(*splitted)
 
 
 def get_path(dir_to_search: str, file_to_search: str):
@@ -18,13 +20,11 @@ def get_path(dir_to_search: str, file_to_search: str):
         return os.path.join(dir_to_search, file_to_search)
 
 
-COMMISSION_WILDBERRIES_CSV: Final[str] = get_path(file_dir, 'niche/commission/Wildberries/WildberriesInput/niche_commission.csv')
-COMMISSION_WILDBERRIES_BINARY: Final[str] = get_path(file_dir,
-                                                     'niche/commission/Wildberries/WildberriesOutput/niche_commission.pickle')
+COMMISSION_WILDBERRIES_CSV: Final[str] = get_path(file_dir, 'niche_commission.csv')
+COMMISSION_WILDBERRIES_BINARY: Final[str] = get_path(file_dir, 'niche_commission.pickle')
 
 WAREHOUSE_WILDBERRIES_JSON: Final[str] = get_path(file_dir, 'data_warehouse.json')
-WAREHOUSE_WILDBERRIES_BINARY: Final[str] = get_path(file_dir,
-                                                    'warehouse/information/Wildberries/WildberriesOutput/warehouse_data.pickle')
+WAREHOUSE_WILDBERRIES_BINARY: Final[str] = get_path(file_dir, 'warehouse_data.pickle')
 
 
 class HandlerType(Enum):
